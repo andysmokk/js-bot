563b99875fdea3a28e6e990896b779b6ab275fcc
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-05-26 22:36:57 +0200

    +

---
+++ js/module-1-11.js
@@ -0,0 +1,11 @@
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  // Change code below this line
+    
+  const totalPrice = orderedQuantity * pricePerItem;
+
+  // Change code above this line
+  console.log(totalPrice)
+  return totalPrice;
+};
+
+calculateTotalPrice(10, 500)
