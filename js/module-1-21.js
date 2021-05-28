a22dfd08a85314e84fafdea79c09e810a1be081c
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-05-28 22:31:31 +0200

    solver task 21

---
+++ js/module-1-21.js
@@ -0,0 +1,12 @@
+function isNumberInRange(start, end, number) {
+    const isInRange = number >= start && number <= end; // Change this line
+    
+    console.log(isInRange);
+
+  return isInRange;
+}
+
+isNumberInRange(10, 30, 17);
+isNumberInRange(10, 30, 5);
+isNumberInRange(20, 50, 24);
+isNumberInRange(20, 50, 76);
